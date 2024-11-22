7cc2a4ecfd40d8b426e6fda8c636e71629c4ef7c
Camilo Weinberger <111007682+CamiloWeinberger@users.noreply.github.com>
2024-11-22 18:58:38 -0300

    eq ready

--- test_pyr.py
+++ test_pyr.py
@@ -2,17 +2,19 @@ from manim import *
 
 class PyrProp(Scene):
     def construct(self):
-        text=MathTex("")
+        text1=MathTex("\Lambda(f_x)")
         text=MathTex(
             "u_3(x\')=",
-            "-\\frac{f_1}{f_2}e^{j\kappa(f_1+f_2)}\mathcal{F}", "[\mathcal{F}[u_1(x)]","","]]",
+            "-\\frac{f_1}{f_2}e^{j\kappa(f_1+f_2)}\mathcal{F}[\mathcal{F}[u_1(x)]",
+            "]",
         )
         text1=MathTex(
-             "u_3(x\')=",
-            "-\\frac{f_1}{f_2}e^{j\kappa(f_1+f_2)}\mathcal{F}", "[\mathcal{F}","\Lambda(f_x)","]]",
+            "u_3(x\')=",
+            "-\\frac{f_1}{f_2}e^{j\kappa(f_1+f_2)}\mathcal{F}[\mathcal{F}[u_1(x)]",
+            "\Lambda(f_x)]",
         )
         
-        self.play(Write(text,))
-        self.wait()
-        self.play(ReplacementTransform(text,text1))
+        self.play(Write(text[0]),Write(text[1]),Write(text[2]))
+        self.wait(2)
+        self.play(Transform(text,text1),)
         self.wait()
