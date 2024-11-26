5d74c6bcb6a8b340bafefdfbb956d5fecf7d58ac
Camilo Weinberger <111007682+CamiloWeinberger@users.noreply.github.com>
2024-11-25 21:31:23 -0300

    m

--- test_pyr.py
+++ test_pyr.py
@@ -17,13 +17,19 @@ class PyrProp(Scene):
         ax = Axes(
             x_range = [0,10],
             y_range = [0,2],       
-        )
-        labels = ax.get_axis_labels()
+        ).set_color(BLACK)
+        labels = ax.get_axis_labels().set_color(BLACK)
         L1x1 = ax.plot(lambda x: 1, x_range=[0,2], color=RED)
         L1x2 = ax.plot(lambda x: 0, x_range=[0,2], color=RED)
-        L2x1 = 
+        L2x1 = ax.plot(lambda x: x/4-2/4, x_range=[2,6], color=RED)
+        L2x2 = ax.plot(lambda x: 1+2/4-x/4, x_range=[2,6], color=RED)
         
-        self.add(ax,labels,L1x1,L1x2)
+        L3x1 = ax.plot(lambda x: 1, x_range=[6,8], color=RED)
+        L3x2 = ax.plot(lambda x: 0, x_range=[6,8], color=RED)
+        L4x1 = ax.plot(lambda x: 2/3, x_range=[6,8], color=RED)
+        L4x2 = ax.plot(lambda x: 1/3, x_range=[6,8], color=RED)
+        
+        self.add(ax,labels,L1x1,L1x2,L2x1,L2x2,L3x1,L3x2,L4x1,L4x2)
         self.play(Write(text[0]),Write(text[1]),Write(text[2]))
         self.wait(2)
         self.play(Transform(text,text1),)
